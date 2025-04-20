d7ef5b2a52d896e2b24f7cea8549a0be0b600c50
Nishanth <nishanth1596@gmail.com>
2025-04-20 19:55:20 +0530

    feat: Initialised project with Tailwind and basic folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Tailwind CSS with React and TypeScript!
-      </h1>
+    <div className="">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,5 @@
+function Main() {
+  return <main className="">Main</main>;
+}
+
+export default Main;
