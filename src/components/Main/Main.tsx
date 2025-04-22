a4a77daf42977390bc13e54503a33a88e352abd6
Nishanth <nishanth1596@gmail.com>
2025-04-22 22:44:07 +0530

    feat: added ErrorMessage component when unknown word is searched

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -42,15 +42,25 @@ function Main() {
   const [data, setData] = useState<DictionaryEntry[]>();
   const [searchInput, setSearchInput] = useState("keyboard");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    setIsLoading(true);
     const getData = async () => {
+      setIsLoading(true);
+      setError("");
       try {
         const newData = await fetchData(searchInput);
-        setData(newData);
+
+        if (!newData || newData.length === 0) {
+          setError("No results found for your search");
+          setData(undefined);
+        } else {
+          setData(newData);
+        }
       } catch (error) {
         console.error("Error fetching dictionary data: ", error);
+        setError("Something went wrong. Please try again");
+        setData(undefined);
       } finally {
         setIsLoading(false);
       }
@@ -66,7 +76,21 @@ function Main() {
   return (
     <main>
       <Search onClick={handleSearch} />
-      <Dictionary data={data} isLoading={isLoading} />
+      {error ? (
+        <article className="mx-6 mt-6 flex flex-col items-center justify-center text-center md:mx-auto md:mt-[50px] md:max-w-96 lg:mx-auto lg:max-w-[46.06rem]">
+          <span>🫤</span>
+          <h3 className="text-Black-800 mt-6 text-xl leading-6 font-bold md:mt-11">
+            No Definitions Found
+          </h3>
+          <p className="text-Gray-500 mt-6 text-lg leading-6 md:mt-8">
+            Sorry pal, we couldn't find definitions for the word you were
+            looking for. You can try the search again at later time or head to
+            the web instead.
+          </p>
+        </article>
+      ) : (
+        <Dictionary data={data} isLoading={isLoading} />
+      )}
     </main>
   );
 }
