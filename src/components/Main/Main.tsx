1aa2147fc8ceca56bbf50f8a3bcd7605ab3b3c70
Nishanth <nishanth1596@gmail.com>
2025-04-24 14:02:49 +0530

    feat: added error component for unkown words

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Dictionary from "./Dicitionary";
 import Search from "./Search";
 import { fetchData } from "../../api/fetchData";
+import ErrorMessage from "../../ui/ErrorMessage";
 
 type LicenseEntry = {
   name: string;
@@ -77,17 +78,7 @@ function Main() {
     <main>
       <Search onClick={handleSearch} />
       {error ? (
-        <article className="mx-6 mt-6 flex flex-col items-center justify-center text-center md:mx-auto md:mt-[50px] md:max-w-96 lg:mx-auto lg:max-w-[46.06rem]">
-          <span>🫤</span>
-          <h3 className="text-Black-800 mt-6 text-xl leading-6 font-bold md:mt-11">
-            No Definitions Found
-          </h3>
-          <p className="text-Gray-500 mt-6 text-lg leading-6 md:mt-8">
-            Sorry pal, we couldn't find definitions for the word you were
-            looking for. You can try the search again at later time or head to
-            the web instead.
-          </p>
-        </article>
+        <ErrorMessage />
       ) : (
         <Dictionary data={data} isLoading={isLoading} />
       )}
---
+++ src/ui/ErrorMessage.tsx
@@ -0,0 +1,17 @@
+function ErrorMessage() {
+  return (
+    <article className="mx-6 mt-6 flex flex-col items-center justify-center text-center md:mx-auto md:mt-[50px] md:max-w-96 lg:mx-auto lg:max-w-[46.06rem]">
+      <span>🫤</span>
+      <h3 className="text-Black-800 mt-6 text-xl leading-6 font-bold md:mt-11">
+        No Definitions Found
+      </h3>
+      <p className="text-Gray-500 mt-6 text-lg leading-6 md:mt-8">
+        Sorry pal, we couldn't find definitions for the word you were looking
+        for. You can try the search again at later time or head to the web
+        instead.
+      </p>
+    </article>
+  );
+}
+
+export default ErrorMessage;
